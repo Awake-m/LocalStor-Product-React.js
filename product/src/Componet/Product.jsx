91f71e024c96e8a62697a692cb82555378e4a739
Awake-m <jagrutimakwana124@gmail.com>
2024-07-03 14:24:09 +0530

    LocalStirage-Product

---
+++ product/src/Componet/Product.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div>
+      <form action="">
+      <h1>Product List</h1>
+        <input type="text" placeholder='Title' /><br/><br/>
+        <input type="text" placeholder='Description' /><br/><br/>
+        <input type="text" placeholder='Price' /><br/><br/>
+        <input type="text" placeholder='Add Image' /><br/><br/>
+        <input type="submit"/>
+      </form>
+    </div>
+  )
+}
+
+export default Product
